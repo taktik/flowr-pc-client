f400d404f4dbd68073038f80a49ffd3e023bc89d
hubert <hj@taktik.com>
2019-10-11 10:13:02 +0200

    FLOW-6315 Keyboard position wexond

--- src/wexond/renderer/app/store/tabs.ts
+++ src/wexond/renderer/app/store/tabs.ts
@@ -404,10 +404,15 @@ export class TabsStore {
   }
 
   public onNewTab() {
-    const { selectedTab } = store.tabs;
-    selectedTab.unselect()
-    store.overlay.isNewTab = true;
-    store.overlay.visible = true;
-    ipcRenderer.send('hide-window');
+    const { selectedTab } = store.tabs
+    if (selectedTab) {
+      selectedTab.unselect()
+    }
+
+    if (! store.overlay.isNewTab || ! store.overlay.visible) {
+      store.overlay.isNewTab = true
+      store.overlay.visible = true
+      ipcRenderer.send('hide-window')
+    }
   }
 }
