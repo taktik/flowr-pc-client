d60e9afe2022be3dd14da65b0ec0554cd237a31f
Loris Mancel <lm@taktik.com>
2020-04-03 18:44:30 +0200

    v1.2.6
    also updated default player values

--- src/frontend/src/playerStore.ts
+++ src/frontend/src/playerStore.ts
@@ -2,17 +2,17 @@ import { IPlayerStore } from './interfaces/playerStore'
 
 export const DEFAULT_PLAYER_STORE: IPlayerStore = {
   streamer: {
-    capacity: 300000, // base streamer capacity, keep small for low definition content
-    maxCapacity: 8000000, // max expansion value for buffer, useful for HD content
-    poolConfig: { poolSize: 5 },
+    capacity: 20000000, // base streamer capacity, keep small for low definition content
+    maxCapacity: 30000000, // max expansion value for buffer, useful for HD content
+    poolConfig: { poolSize: 2 },
     sendInterval: 300, // to increase on "BUFFER ERROR" in flowr
   },
   decryption: { use: true }, // whether to use ts-decryptor
   tsDecryptor: {
     alignConfig: {
-      capacity: 700e3, // must be superior or equal to UdpStreamer's capacity
-      maxCapacity: 1e6, // must be superior or equal to UdpStreamer's maxCapacity
-      poolConfig: { poolSize: 200 }, // increase this if there are alignment errors
+      capacity: 16384, // must be superior or equal to UdpStreamer's capacity
+      maxCapacity: 30000, // must be superior or equal to UdpStreamer's maxCapacity
+      poolConfig: { poolSize: 100 }, // increase this if there are alignment errors
     },
     formatterConfig: {
       leftoversConfig: { allocatedMemory: 5e6 }, // increase this if there are artefacts
@@ -21,8 +21,8 @@ export const DEFAULT_PLAYER_STORE: IPlayerStore = {
   },
   udpStreamer: {
     allowOverwrite: true,
-    capacity: 700e3, // base streaming "speed"
-    maxCapacity: 1e6, // max streaming "speed", to increase on OverflowErrors
-    poolConfig: { poolSize: 400 }, // per stream output, to increase on alignment errors
+    capacity: 16384, // base streaming "speed"
+    maxCapacity: 30000, // max streaming "speed", to increase on OverflowErrors
+    poolConfig: { poolSize: 65000 }, // per stream output, to increase on alignment errors
   },
 }
