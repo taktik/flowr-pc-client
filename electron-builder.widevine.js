5c10d6d979e3bc9475f9d423371cfa704a6ed274
lmancel <lmancel@centrale-marseille.fr>
2022-02-14 14:29:54 +0100

    Bugfix/widevine (#47)
    * First attempt to automate VMP signing
    
    * fixes for Windows
    
    * removed useless Widevine config
    
    * clarified PYTHON3 env var

--- electron-builder.widevine.js
+++ electron-builder.widevine.js
@@ -1,9 +1,33 @@
+/* eslint-disable */
+
 const common = require('./electron-builder.common')
+const { exec } = require('child_process')
+
+function signIf(targetPlatform) {
+  return ({ appOutDir, electronPlatformName }) => {
+    if (electronPlatformName !== targetPlatform) {
+      return
+    }
+
+    return new Promise((resolve, reject) => {
+      exec(`${process.env.PYTHON3} -m castlabs_evs.vmp sign-pkg ${appOutDir}`, (err) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+  }
+}
 
 module.exports = {
   ...common,
   artifactName: '${productName}-${version}-${os}-${arch}-widevine.${ext}',
+  afterPack: signIf('darwin'),
+  afterSign: signIf('win32'),
+  electronVersion: '11.1.1',
   electronDownload: {
+    version: '11.1.1-wvvmp',
     mirror: 'https://github.com/castlabs/electron-releases/releases/download/v'
   }
 }
