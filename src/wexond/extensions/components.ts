8794aac969d5acb62ae420c5d3313f8c44871a31
Loris Mancel <lm@taktik.com>
2022-12-15 12:41:00 +0100

    fix(components) don't use components if not widevine
    only castlab's version of electron exports the "components" module
    Attempting to use it on the "normal" version throws an error

--- src/wexond/extensions/components.ts
+++ src/wexond/extensions/components.ts
@@ -5,7 +5,22 @@ const log = getLogger('Components')
 
 let initializing: Promise<void[]> | undefined
 
+/**
+ * castlab's electron-widevine provides a "component" extension
+ * Only available on widevine electron, it will be undefined otherwise
+ * @documentation https://github.com/castlabs/electron-releases/blob/master/docs/api/components.md
+ */
+declare module 'electron' {
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  const components: undefined | {
+    whenReady(): Promise<void[]>
+  }
+}
+
 export default async function initComponents(): Promise<void> {
+  if (!components) {
+    return
+  }
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   if (initializing) {
     await initializing
