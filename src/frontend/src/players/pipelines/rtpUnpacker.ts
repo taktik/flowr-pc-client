af43b7bdf6c691791312569f4a5e74021892e22e
philippe_mertens <pme@taktik.com>
2023-02-16 11:57:21 +0100

    Various improvements and cleaning

--- src/frontend/src/players/pipelines/rtpUnpacker.ts
+++ src/frontend/src/players/pipelines/rtpUnpacker.ts
@@ -1,73 +1,62 @@
-import { Transform, TransformCallback, TransformOptions } from "stream"
-
-export class RtpPacket {
-  constructor(
-    public payloadType: number,
-    public sequenceNumber: number,
-    public timestamp: number,
-    public ssrc: number,
-    public payload: Buffer
-  ) {}
-}
-
-
+import { Transform, TransformCallback, TransformOptions } from 'stream'
 
 /**
- * This class implements a Transform stream. 
- * It is meant to be used as a Rtp packet payload unpacker for MPEGTS (MPEG Transport Stream specified by MPEG-2 part 1). 
- * Currently, it assumes that each chunk represent one UDP packet of OZI Transport Layer that embed one whole RTP packet of OZI Application Layer.
+    This class implements a Transform stream responsible for unpacking MPEG-TS, MPEG-1 (A/V) and MPEG-2 (A/V) media payload from RTP packet.
+    For any other payload types, the full RTP packet will be transfered as is for FFMPEG to deal with.
+
+    Notes:
+
+      MPEG-1/2 Audio payloads are identified by the Payload Type (PT) value of the RTP packet header: PT = 14
+      MPEG-1/2 Video payloads are identified by the Payload Type (PT) value of the RTP packet header: PT = 32
+      MPEG-TS Audio & Video payloads are identified by the Payload Type (PT) value of the RTP packet header: PT = 33
+
+      According to the specification, MPEG-1/2 payload are simple chunks of stream bytes
+      According to the corresponding, the content of a RTP packet will always contain an integral number of MPEG-TS packets
+
+    Refs: 
+
+      https://en.wikipedia.org/wiki/Real-time_Transport_Protocol#:~:text=The%20RTP%20header%20has%20a,the%20particular%20class%20of%20application
+      https://datatracker.ietf.org/doc/html/rfc2250
  */
+
+const UNPACKED_PAYLOAD_TYPES: { [key: number]: string } = {
+  14: 'MPEG-1/2 Audio',
+  32: 'MPEG-1/2 Video',
+  33: 'MPEG-TS Audio & Video',
+}
 export class RtpUnpacker extends Transform {
   constructor(options: TransformOptions = {}) {
     super(options)
   }
 
-  _transform(chunk: Buffer, _encoding: string, callback: TransformCallback): void {
+  _transform(
+    chunk: Buffer,
+    _encoding: string,
+    callback: TransformCallback,
+  ): void {
     const rtpHeader = chunk.slice(0, 12)
     const hasPadding = Boolean(rtpHeader[0] & 0x20)
-    const marker = (rtpHeader[1]) >>> 7;
     const payloadType = rtpHeader[1] & 0x7f
 
-    //  If the payload does not correspond to
-    //    1. A full <=> Marker = M = 1 
-    //    2. Audio/Video MPEG2 packet <=> Payload Type = PT = 33
-    //  the packet is left unchanged and passed through for FFmpeg to deal with.
-    //  Ref: https://en.wikipedia.org/wiki/Real-time_Transport_Protocol#:~:text=The%20RTP%20header%20has%20a,the%20particular%20class%20of%20application.
-    if(payloadType !== 33 || marker !== 1){
+    if (!UNPACKED_PAYLOAD_TYPES[payloadType]) {
       callback(null, chunk)
     } else {
-      const sequenceNumber = rtpHeader.readUInt16BE(2)
-      const timestamp = rtpHeader.readUInt32BE(4)
-      const ssrc = rtpHeader.readUInt32BE(8)
-
       let payloadStart = 12
       let extensionLength = 0
+
       if (rtpHeader[0] & 0x10) {
         // Has Extension
         extensionLength = rtpHeader.readUInt16BE(12)
         payloadStart += 4 + extensionLength
       }
 
-      if (hasPadding) {
-        const paddingLength = chunk[chunk.length - 1]
-        if (paddingLength > 0) {
-          // Has Padding
-          const packetLength = chunk.length
-          const lastByteIndex = packetLength - 1
-          const paddingStart = packetLength - paddingLength
-          if (paddingStart > payloadStart) {
-            // Padding bytes should be zeroed out
-            chunk.fill(0, paddingStart, lastByteIndex)
-          }
-          chunk = chunk.slice(0, paddingStart)
-        }
-      }
-
-      const payloadEnd = chunk.length
+      //  If padding is present, its length is encoded in the chunk's last byte: chunk[chunk.length - 1]
+      const payloadEnd = hasPadding
+        ? chunk.length - chunk[chunk.length - 1]
+        : chunk.length
       const payload = chunk.slice(payloadStart, payloadEnd)
-      const packet = new RtpPacket(payloadType, sequenceNumber, timestamp, ssrc, payload)
 
-      callback(null, packet.payload)
+      callback(null, payload)
     }
   }
 }
